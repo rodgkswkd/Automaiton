f89d8b313bf93b334d222aeb5fd0033cab7dd8f6
unknown <theo.kim@hq.nsuslab.net>
2022-06-20 12:20:23 +0900

    first push

---
+++ types/wdio.d.ts
@@ -0,0 +1,14 @@
+// wdio.d.ts
+declare global {
+  namespace WebdriverIO {
+    interface Browser {
+      // browserCustomCommand: (arg: any) => Promise<void>
+    }
+    interface MultiRemoteBrowser {
+      // browserCustomCommand: (arg: any) => Promise<void>
+    }
+
+    interface Element {
+      // elementCustomCommand: (arg: any) => Promise<number>
+    }
+  }
